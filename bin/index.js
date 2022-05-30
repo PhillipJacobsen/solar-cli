74177b0d0576dbc64cd53293378654c98cd81171
PJ <33669966+PhillipJacobsen@users.noreply.github.com>
2022-05-30 17:51:02 -0600

    added ipfs tx and error handling

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 const package = require("../package.json");
+const isIPFS = require("is-ipfs")   // used for verifing valid IPFS hash
 const nodeIP = package.nodeIP;      // retrieve ip address of relay node from package.json
 const network = package.network;    // retrieve network type (mainnet or testnet) from package.json
 const chalk = require("chalk");
@@ -19,14 +20,26 @@ const Connection = Client.Connection;
 const client = new Connection(nodeIP);
 
 
+function handleError(err) {
+    console.log(errorColor("oh crap."));
+    console.log(errorColor(err));
+}
+
 async function connectRelay() {
-    console.log(infoColor("Opening", network, "connection to relay:", nodeIP));
+    console.log(infoColor("Opening", network, "connection to relay:", "nodeIP"));
+
     Managers.configManager.setFromPreset(network);   //set the network (testnet or mainnet)
-    const blockchain = await client.get("blockchain");
-    Managers.configManager.setHeight(blockchain.body.data.block.height);
-    // get current blockchain height
-    // const blockchain = await client.get("blockchain");
-    // const bridgechainHeight = blockchain.body.data.block.height;
+    try {
+        const blockchain = await client.get("blockchain");
+        Managers.configManager.setHeight(blockchain.body.data.block.height);
+        return true
+
+    } catch (err) {
+        console.log(errorColor(err));
+        console.log(errorColor("Cannot connect to relay node"));
+        return false
+    }
+    // const blockchain = await client.get("blockchain").catch(handleError);
 }
 
 (async () => {
@@ -54,10 +67,19 @@ async function connectRelay() {
         command: "relay",
         describe: "Get status of relay node used for accessing blockchain",
         async handler(argv) {
-            connectRelay()
             console.log(infoColor("Retrieving relay node status"));
-            const response = await client.api("node").status();
-            console.log(resultColor("%s"), response.body.data);
+
+            if (await connectRelay()) {
+                try {
+                    const response = await client.api("node").status();
+                    console.log(resultColor("%s"), response.body.data);
+                } catch (err) {
+                    console.log(errorColor(err));
+                    console.log(errorColor("Cannot retrieve node status"))
+                }
+            } else {
+
+            }
         }
     }
     )
@@ -102,8 +124,19 @@ async function connectRelay() {
         },
         async handler(argv) {
             console.log(infoColor("Retrieving Peers"));
-            const peers = await client.api("peers").all();
-            console.log(peers.body.data);
+
+            if (await connectRelay()) {
+                try {
+                    const peers = await client.api("peers").all();
+                    //console.log(peers.body.data);
+                    console.log(JSON.stringify(peers.body, null, 4))
+                } catch (err) {
+                    console.log(errorColor(err));
+                    console.log(errorColor("Cannot retrieve peer list"))
+                }
+            } else {
+
+            }
         }
     }
     )
@@ -123,11 +156,21 @@ async function connectRelay() {
             }
         },
         async handler(argv) {
-            connectRelay()
             console.log(infoColor("Retrieving Nonce from wallet %s"), argv.adr);
-            const wallet = await client.api("wallets").get(argv.adr);
-            const nonce = wallet.body.data.nonce;
-            console.log(resultColor("Nonce: %s"), nonce);
+
+            if (await connectRelay()) {
+                try {
+                    const wallet = await client.api("wallets").get(argv.adr);
+                    const nonce = wallet.body.data.nonce;
+                    console.log(resultColor("Nonce: %s"), nonce);
+                } catch (err) {
+                    console.log(errorColor(err));
+                    console.log(errorColor("Cannot retrieve nonce"))
+                }
+            } else {
+
+            }
+
         }
     }
     )
@@ -251,6 +294,11 @@ async function connectRelay() {
                 demandOption: true,
                 type: "string"
             },
+            amt: {
+                describe: "Amount of coins to send",
+                demandOption: true,
+                type: "string"
+            },
             passphrase: {
                 describe: "Your Private Passphrase(12 words)",
                 demandOption: true,
@@ -264,15 +312,26 @@ async function connectRelay() {
 
         },
         async handler(argv) {
-            connectRelay();
+
+            if (!(await connectRelay())) {
+                return
+            }
 
             const passphrase = argv.passphrase;
             const senderWalletAddress = Identities.Address.fromPassphrase(passphrase);
             const recipientWalletAddress = argv.adr;
-
+            const amount = argv.amt;
+            
             // Step 1: Retrieve the nonce of the sender wallet and increment
-            const senderWallet = await client.api("wallets").get(senderWalletAddress);
-            const senderNonce = Utils.BigNumber.make(senderWallet.body.data.nonce).plus(1);
+            let senderNonce;
+            try {
+                const senderWallet = await client.api("wallets").get(senderWalletAddress);
+                senderNonce = Utils.BigNumber.make(senderWallet.body.data.nonce).plus(1);
+            } catch (err) {
+                console.log(errorColor(err));
+                console.log(errorColor("Cannot retrieve nonce"))
+            }
+
             let transaction = {};
 
             // Step 2: Create and Sign the transaction
@@ -282,7 +341,7 @@ async function connectRelay() {
                     .version(3)
                     .nonce(senderNonce.toFixed())
                     .recipientId(recipientWalletAddress)
-                    .amount(1)
+                    .amount(amount)
                     .fee(3000000)
                     .vendorField(argv.smartbridge)
                     .sign(passphrase);
@@ -292,7 +351,7 @@ async function connectRelay() {
                     .version(3)
                     .nonce(senderNonce.toFixed())
                     .recipientId(recipientWalletAddress)
-                    .amount(1)
+                    .amount(amount)
                     .fee(3000000)
                     .sign(passphrase);
             }
@@ -320,6 +379,103 @@ async function connectRelay() {
     )
 
 
+
+
+    /* 
+    Command: Send IPFS transaction
+    */
+    yargs.command({
+        command: "tx-ipfs",
+        describe: "Send IPFS transaction with optional smartbridge message",
+        builder: {
+            hash: {
+                describe: "IPFS Hash",
+                demandOption: true,
+                type: "string"
+            },
+            passphrase: {
+                describe: "Your Private Passphrase(12 words)",
+                demandOption: true,
+                type: "string"
+            },
+            smartbridge: {
+                describe: "Message to include with transaction(optional)",
+                demandOption: false,
+                type: "string"
+            },
+
+        },
+        async handler(argv) {
+
+            if (!(await connectRelay())) {
+                return
+            }
+            const passphrase = argv.passphrase;
+            const senderWalletAddress = Identities.Address.fromPassphrase(passphrase);
+            const ipfsHash = argv.hash;
+
+            // verify if Hash is valid
+            if (!(isIPFS.cid(ipfsHash))) {
+                console.log(errorColor("Not a valid IPFS hash"));
+                return
+            }
+
+            // Step 1: Retrieve the nonce of the sender wallet and increment
+            let senderNonce;
+            try {
+                const senderWallet = await client.api("wallets").get(senderWalletAddress);
+                senderNonce = Utils.BigNumber.make(senderWallet.body.data.nonce).plus(1);
+            } catch (err) {
+                console.log(errorColor(err));
+                console.log(errorColor("Cannot retrieve nonce"))
+            }
+
+            let transaction = {};
+
+            // Step 2: Create and Sign the transaction
+            if ('smartbridge' in argv) {
+                console.log("smartbridge exists");
+                transaction = Transactions.BuilderFactory.ipfs()
+                    .version(3)
+                    .nonce(senderNonce.toFixed())
+                    .ipfsAsset(ipfsHash)
+                    .fee(3000000)
+                    .vendorField(argv.smartbridge)
+                    .sign(passphrase);
+            } else {
+                console.log("smartbridge does not exist");
+                transaction = Transactions.BuilderFactory.ipfs()
+                    .version(3)
+                    .nonce(senderNonce.toFixed())
+                    .ipfsAsset(ipfsHash)
+                    .fee(3000000)
+                    .sign(passphrase);
+            }
+
+            // Step 3: Broadcast the transaction
+            console.log(infoColor("Sending transaction..."));
+            const broadcastResponse = await client.api("transactions").create({ transactions: [transaction.build().toJson()] });
+            // console.log(JSON.stringify(broadcastResponse.body, null, 4))
+            if (broadcastResponse.status == 200) {
+                const accept = broadcastResponse.body.data.accept;
+                if (!(accept.length === 0)) {
+                    const txid = broadcastResponse.body.data.accept[0];
+                    console.log(resultColor("Transaction ID: %s"), txid);
+                } else {
+                    var invalidID = broadcastResponse.body.data.invalid[0];
+                    var errormessage = broadcastResponse.body.errors[invalidID].message;
+                    console.log(errorColor("Error Message: %s"), errormessage);
+                }
+            } else {
+                console.log(errorColor("Error sending. Status code: %s"), broadcastResponse.status);
+            }
+        }
+    }
+    )
+
+    process.on("uncaughtException", (err) => {
+        console.log(errorColor("UncaughtException %s"), err);
+    })
 
 
     yargs.parse()
